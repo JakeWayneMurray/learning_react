50311700373f133cc15f57f01eb49b19315defab
Jake Murray <109402063+JakeWayneMurray@users.noreply.github.com>
2024-12-25 16:13:19 -0500

    vercel help

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,17 @@ import { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
+import { ErrorBoundary } from 'react-error-boundary';
+
+function ErrorFallback({error}) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+    </div>
+  )
+}
+
 function App() {
 
   const [showProductList, setShowProductList] = useState(false);
@@ -15,24 +26,26 @@ function App() {
   };
 
   return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image" />
-        <div className="content">
-            <h1 style={{marginTop:100}}> Welcome To Paradise Nursery</h1>
-            <div className="divider"></div>
-            <p>Where Green Meets Serenity</p>
-            <button className="get-started-button" onClick={handleGetStartedClick}>
-              Get Started
-            </button>
-          <AboutUs />
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <div className="app-container">
+        <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+          <div className="background-image" />
+          <div className="content">
+              <h1 style={{marginTop:100}}> Welcome To Paradise Nursery</h1>
+              <div className="divider"></div>
+              <p>Where Green Meets Serenity</p>
+              <button className="get-started-button" onClick={handleGetStartedClick}>
+                Get Started
+              </button>
+            <AboutUs />
 
+          </div>
+        </div>
+        <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+          <ProductList onClose={handleCloseProductList} />
         </div>
       </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList onClose={handleCloseProductList} />
-      </div>
-    </div>
+    </ErrorBoundary>
   );
 }
 
